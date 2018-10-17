b2dae71618416819bca7fbc3c8aa921a8dcb87f5
Felix Javier Nuñez Gonzalez <fgonzalez@utdelacosta.edu.mx>
2018-10-17 15:31:36 -0600

    Proyecto en pagina 203

--- src/java/com/nunez/servlets/Test.java
+++ src/java/com/nunez/servlets/Test.java
@@ -1,31 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.nunez.servlets;
 
-import com.nunez.HibernateHelper;
+import com.nunez.Categoria;
 import com.nunez.Libro;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  *
  * @author nunez-pc
  */
 public class Test {
+
     public static void main(String[] args) {
-        SessionFactory factoriaSession = HibernateHelper.getSessionFactory();
-        List<Libro> listaDeLibros;
-        try (Session session = factoriaSession.openSession()) {
-            listaDeLibros = session.createQuery("from Libro libro right join fetch libro.categoria").list();
-            for (Libro listaDeLibro : listaDeLibros) {
-                System.out.println(listaDeLibro.getTitulo());
-            }
-        }
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ArquitecturaJava");
+        EntityManager em = emf.createEntityManager();
+        /*TypedQuery<Libro> consulta = em.createQuery("Select l from Libro l", Libro.class);
+        List<Libro> lista = consulta.getResultList();
+        for (Libro l : lista) {
+            System.out.println(l.getTitulo());
+        }*/
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         
-        
+        Libro libro = new Libro("8", "IOS", new Categoria(1));
+        em.persist(libro);
+		tx.commit();
+		em.close();
+
     }
 }
